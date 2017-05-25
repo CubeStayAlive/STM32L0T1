68ab13588aff03311a67acf5e82ef10d51378fc2
BerndDammer <BerndDammer@web.de>
2017-05-25 23:13:03 +0200

    Stm Studio

---
+++ StartStudio.bat
@@ -0,0 +1,5 @@
+rem e:\STM32\jre64\bin\javaw.exe -jar e:\STM32\STM32STUDIO\STMStudio.jar e:\STM32\workspace_f4\STM32L053\ShowCounter.tsp
+rem start e:\STM32\jre32\bin\javaw.exe -jar e:\STM32\STM32STUDIO\STMStudio.jar e:\STM32\workspace_f4\STM32L053\ShowCounter.tsp
+
+cd /D e:\STM32\STM32STUDIO
+start e:\STM32\jre32\bin\javaw.exe -jar e:\STM32\STM32STUDIO\STMStudio.jar
--- appl/tasks/ThreadCDC.c
+++ appl/tasks/ThreadCDC.c
@@ -13,10 +13,20 @@
 #include "main.h"
 #include "usbd_cdc_if.h"
 
-static int counter_step1 = 0;
-static int counter_step2 = 0;
-static bool is_connected = false;
-static bool is_bauded = false;
+static struct
+{
+	int counter_step1;
+	int counter_step2;
+	bool is_connected;
+	bool is_bauded;
+}stms_cdc
+=
+{
+	0,
+	0,
+	false,
+	false,
+};
 
 void ThreadCDC(void const * argument)
 {
@@ -27,40 +37,42 @@ void ThreadCDC(void const * argument)
 
 	HAL_GPIO_WritePin(USB_FS_Mark_GPIO_Port, USB_FS_Mark_Pin, GPIO_PIN_RESET);
 
-	while(step1)
+	while (step1)
 	{
 		HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_SET);
 		vTaskDelay(100);
 		HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
 		vTaskDelay(1200);
-		if( ! HAL_GPIO_ReadPin(B1_GPIO_Port, B1_Pin))
+		if (!HAL_GPIO_ReadPin(B1_GPIO_Port, B1_Pin))
 		{
 			step1 = false;
 		}
-		counter_step1++;
+		stms_cdc.counter_step1++;
 	}
 
 	// activate USB Full Speed Identification
 	HAL_GPIO_WritePin(USB_FS_Mark_GPIO_Port, USB_FS_Mark_Pin, GPIO_PIN_SET);
 
-	while(step2)
+	while (step2)
 	{
 		HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_SET);
 		vTaskDelay(700);
 		HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
 		vTaskDelay(300);
-		if( is_connected )
+		if (stms_cdc.is_connected)
 		{
 			step2 = false;
 		}
-		counter_step2++;
+		stms_cdc.counter_step2++;
 	}
-	while(true)
+	while (true)
 	{
 		int pcount = 1;
-		if( is_connected) pcount++;
-		if( is_bauded) pcount++;
-		for( int i = 0; i < pcount; i++)
+		if (stms_cdc.is_connected)
+			pcount++;
+		if (stms_cdc.is_bauded)
+			pcount++;
+		for (int i = 0; i < pcount; i++)
 		{
 			HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_SET);
 			vTaskDelay(150);
@@ -73,23 +85,23 @@ void ThreadCDC(void const * argument)
 
 void CBConnect(void)
 {
-	is_connected = true;
-	is_bauded = false;
+	stms_cdc.is_connected = true;
+	stms_cdc.is_bauded = false;
 }
 void CBDisconnect(void)
 {
-	is_connected = false;
-	is_bauded = false;
+	stms_cdc.is_connected = false;
+	stms_cdc.is_bauded = false;
 }
 
 void CBBaud(void)
 {
-	is_bauded = true;
+	stms_cdc.is_bauded = true;
 }
 
 static unsigned char answer[] = "Hello\015\012";
 
 void CBIncomming(void)
 {
-	CDC_Transmit_FS( answer, sizeof( answer));
+	CDC_Transmit_FS(answer, sizeof(answer));
 }
